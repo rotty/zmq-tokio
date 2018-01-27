f807cc65fd5ec06dfde1da33c9d27d80659ab679
Joaquin Rosales <globojorro@gmail.com>
2018-01-26 19:34:12 -0600

    define SocketRecv trait to have a method API

--- src/lib.rs
+++ src/lib.rs
@@ -425,3 +425,37 @@ pub trait SocketSend {
         I: IntoIterator<Item = T>,
         T: Into<Message>;
 }
+
+/// API methods for receiving messages with sockets.
+pub trait SocketRecv {
+    /// Return true if there are more frames of a multipart message to receive.
+    fn get_rcvmore(&self) -> io::Result<bool>;
+
+    /// Receive a message into a `Message`. The length passed to `zmq_msg_recv` is the length
+    /// of the buffer.
+    fn recv(&self, &mut Message, i32) -> io::Result<()>;
+
+    /// Receive bytes into a slice. The length passed to `zmq_recv` is the length of the slice. The
+    /// return value is the number of bytes in the message, which may be larger than the length of
+    /// the slice, indicating truncation.
+    fn recv_into(&self, &mut [u8], i32) -> io::Result<usize>;
+
+    /// Receive a message into a fresh `Message`.
+    fn recv_msg(&self, i32) -> io::Result<Message>;
+
+    /// Receive a message as a byte vector.
+    fn recv_bytes(&self, i32) -> io::Result<Vec<u8>>;
+
+    /// Receive a `String` from the socket.
+    ///
+    /// If the received message is not valid UTF-8, it is returned as the original `Vec` in the `Err`
+    /// part of the inner result.
+    fn recv_string(&self, i32) -> io::Result<Result<String, Vec<u8>>>;
+
+    /// Receive a multipart message from the socket.
+    ///
+    /// Note that this will allocate a new vector for each message part; for many applications it
+    /// will be possible to process the different parts sequentially and reuse allocations that
+    /// way.
+    fn recv_multipart(&self, i32) -> io::Result<Vec<Vec<u8>>>;
+}
